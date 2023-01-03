b2c354da1350cf3d78e47c41de47c2d3fafe80d7
DragonSoul7595 <121900362+Ange1-V@users.noreply.github.com>
2023-01-03 20:35:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+user_response = input("Hello, how are you?\n")
+if user_response == "good" or user_response == "Good":
+  print("That's good!")
+elif user_response == "bad" or user_response == "Bad":
+  print("I'm sorry to hear that")
+else:
+  print("Sorry I didn't quite get that")
